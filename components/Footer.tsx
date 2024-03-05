fa0fadd9c4a7a6f72eaba5d1f954f8b304c0557e
Agada Francis Louis N <agadafrancis91@gmail.com>
2024-03-05 17:32:27 +0100

    still on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,7 +16,20 @@ const Footer = () => {
             </div>
 
             <div className="footer__links">
-
+              {footerLinks.map((link ) => (
+                <div key={link.title} className="footer__link">
+                  <h3 className="font-bold">{link.title}</h3>
+                  {link.links.map((item) =>(
+                    <Link 
+                      key={item.title}
+                      href={item.url}
+                      className="text-gray-500"
+                    >
+                      {item.title}
+                    </Link>
+                  ))}
+                </div>
+              ))}
             </div>
         </div>
     </footer>
