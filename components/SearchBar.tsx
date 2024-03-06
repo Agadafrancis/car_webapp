df7e606643c5e58e80e3226d17aaf43c614684ac
Agada Francis Louis N <agadafrancis91@gmail.com>
2024-03-06 15:56:18 +0100

    still on SearchBar, SearchManufacturer and Headless UI

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+'use client';
+
+import { useState } from "react";
+
+import { SearchManufacturer } from ".";
 
 const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState ('')
+
+    const handleSearch = () => {}
+
   return (
-    <div>
-      
-    </div>
+    <form className="searchbar" onSubmit={handleSearch}>
+        <div className="searchbar__item">
+            <SearchManufacturer
+                manufacturer={manufacturer}
+                setManufacturer={setManufacturer}
+            />
+        </div>
+    </form>
   )
 }
 
---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,12 @@
+import { SearchManufacturerProps } from "@/types"
+
+
+const SearchManufacturer = ( {manufacturer, setManufacturer}: SearchManufacturerProps) => {
+  return (
+    <div>
+      
+    </div>
+  ) 
+}
+
+export default SearchManufacturer
