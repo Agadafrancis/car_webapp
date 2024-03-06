ced3f3da36193613e04a8a035b29b910750457e5
Agada Francis Louis N <agadafrancis91@gmail.com>
2024-03-06 15:04:46 +0100

    creating the body
    custom filter and search bar components created. created under the components folder, imported and exported inside the index.ts (components) file, then exported in the page.tsx file

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -31,7 +31,20 @@ const Footer = () => {
                 </div>
               ))}
             </div>
-        </div>
+            </div>
+
+            <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+              <p>@2024 Carhub LT. All Rights Reserved</p>
+              
+              <div className="footer__copyrights-link">
+                    <Link href="/" className="text-gray-500">
+                      Privacy Policy
+                    </Link>
+                    <Link href="/" className="text-gray-500">
+                      Terms of Use
+                    </Link>
+              </div>
+            </div>
     </footer>
   )
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,12 +28,12 @@ const Hero = () => {
                 handleClick={handeScroll}
             />
         </div>
-        <div className="hero__container">
+        <div className="hero__image-container">
             <div className="hero__image">
                 <Image src="/hero.png" alt="hero" fill className="object-contain" />
+            </div>
 
                 <div className="hero__image-overlay" />
-            </div>
         </div>
     </div>
   )
